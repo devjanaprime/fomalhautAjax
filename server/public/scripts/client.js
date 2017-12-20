7a597d5e1715978305a7053cb61057926ea2e93e
devjanaprime <dev@primeacademy.io>
2017-12-20 15:21:20 -0600

    added AJAX get

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,26 @@
 console.log( 'JS' );
 
 $( document ).ready( function(){
-    console.log( 'JQ' );
+    getMessages();
 });
+
+function displayMessages( messages ){
+    console.log( 'in displayMessages:', messages );
+    let $el = $( '#messagesOut' );
+    $el.empty();
+    for( let i=0; i< messages.length; i++ ){
+        $el.append( '<li>"' + messages[i].message + '" - ' + messages[i].from + '</li>')
+    } // end for
+}
+
+function getMessages(){
+    console.log( 'in getMessages' );
+    $.ajax({
+        method: 'GET',
+        url: '/messages',
+        success: function( response ){
+            console.log( 'back from server with:', response );
+            displayMessages( response );
+        }
+    });
+} // end getMessages
